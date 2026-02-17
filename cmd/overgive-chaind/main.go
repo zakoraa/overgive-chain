e689feeff48bac69886d8b6ddf50cba06369c0f2
Developer Experience team at Ignite <hello@ignite.com>
2026-02-17 23:58:39 +0700

    Initialized with Ignite CLI

---
+++ cmd/overgive-chaind/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	clienthelpers "cosmossdk.io/client/v2/helpers"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
+	"overgive-chain/app"
+	"overgive-chain/cmd/overgive-chaind/cmd"
+)
+
+func main() {
+	rootCmd := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, clienthelpers.EnvPrefix, app.DefaultNodeHome); err != nil {
+		fmt.Fprintln(rootCmd.OutOrStderr(), err)
+		os.Exit(1)
+	}
+}
